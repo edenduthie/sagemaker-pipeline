d5891f2215f301b868bb376e05c7de17c3e80737
Eden Duthie <eduthie@gmail.com>
2018-11-08 09:10:53 +1100

    Changed to us-east-1

--- Chalice/invoke.py
+++ Chalice/invoke.py
@@ -3,7 +3,7 @@ import json
 import numpy as np
 
 file_name = 'floppy.jpg'
-endpoint_name = 'DEMO-imageclassification-ep--2018-04-23-19-55-49'
+endpoint_name = 'prod-smp-d3cb466-2018-11-07-03-13-1'
 runtime = boto3.Session().client(service_name='runtime.sagemaker',region_name='us-east-1')
 
 with open(file_name, 'rb') as f:
